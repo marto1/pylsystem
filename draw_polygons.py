5498a74d79d6335fc35e41d5b8aa7992624bf8c8
Martin Gergov <martingergov1@gmail.com>
2018-06-21 22:06:14 +0200

    randomize polygon creation from straight line

--- draw_polygons.py
+++ draw_polygons.py
@@ -135,20 +135,50 @@ def draw(surface, bg_surface):
     draw_it(fordraw, surface)
     pygame.display.flip()
     
+# def main():
+#     pygame.init()
+#     screen_width, screen_height = (1024, 600)
+#     size = (screen_width,screen_height)
+#     clock = pygame.time.Clock()
+#     pygame.display.set_caption("Generate polygons")
+#     surface = pygame.display.set_mode(size)
+#     bg_surface = pygame.Surface(size, pygame.SRCALPHA)
+#     bg_surface.fill((0, 0,0))
+#     ev = LoopingCall(draw, surface, bg_surface)
+#     ev.start(1.0 / 2)
+#     stdio.StandardIO(ChatboxProtocol())
+#     reactor.run()
+from random import choice, randint
+
+def randomize():
+    global axiom
+    alpha = ["F"]
+    for i in range(9):
+        alpha.append(choice(["", "R", "L"]))
+        alpha.append("F")
+    alpha = "".join(alpha)
+    axiom = "P{}".format(alpha)
+    #("".join([choice(alpha) for i in xrange(n)]))
+    print axiom
+
+
 def main():
     pygame.init()
     screen_width, screen_height = (1024, 600)
     size = (screen_width,screen_height)
     clock = pygame.time.Clock()
-    pygame.display.set_caption("Generate polygons")
+    pygame.display.set_caption("L-system generate")
     surface = pygame.display.set_mode(size)
     bg_surface = pygame.Surface(size, pygame.SRCALPHA)
     bg_surface.fill((0, 0,0))
     ev = LoopingCall(draw, surface, bg_surface)
     ev.start(1.0 / 2)
+    ev2 = LoopingCall(randomize)
+    ev2.start(0.3)
     stdio.StandardIO(ChatboxProtocol())
     reactor.run()
-        
+
+
 if __name__ == '__main__':
     main()
     
